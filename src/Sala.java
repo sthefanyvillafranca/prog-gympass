0bae15a335453cf8dddb96f03fcfcdd0de701933
sthefanyvillafranca <svillsei@myuax.com>
2026-02-23 23:00:43 +0100

    Creacion de la clase Sala con atributos, constructor y toString

---
+++ src/Sala.java
@@ -0,0 +1,20 @@
+public class Sala {
+    private int codigoSala;
+    private String descripcion;
+
+    public Sala(int codigoSala, String descripcion) {
+        this.codigoSala = codigoSala;
+        this.descripcion = descripcion;
+    }
+    public int getCodigoSala() {
+        return codigoSala;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return codigoSala + " - " + descripcion;
+    }
+}
