f9ac86c743c7a8cb3c3d298d3d7ee12eaddaf452
sthefanyvillafranca <svillsei@myuax.com>
2026-02-23 12:36:40 +0100

    clase socio creado

---
+++ src/Socio.java
@@ -0,0 +1,11 @@
+public class Socio {
+    private final int numSocio;
+    private final String nombre;
+    // commit y push para cambios.
+    public Socio(int numSocio, String nombre) {
+        this.numSocio = numSocio;
+        this.nombre = nombre;
+
+    }
+}
+
