bf777f330268b222fafd03ddb6d2243342efdf3d
sthefanyvillafranca <svillsei@myuax.com>
2026-02-25 08:47:42 +0100

    Registrar socio en Gimnasio y primer intento de creación expulsarSocio

--- src/Gimnasio.java
+++ src/Gimnasio.java
@@ -1,4 +1,42 @@
 package PACKAGE_NAME;
 
 public class Gimnasio {
+    private String codigo;
+    private String nombre;
+    private Socio[] listaSocios = new Socio[12];
+    private Sala[] listaSalas = new Sala[6];
+    private int responsableId = -1;
+
+
+    public Gimnasio(String codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    public boolean registrarSocio(Socio s){
+        boolean registrado = false;
+        for (int i = 0; i < listaSocios.length; i++) {
+            if (listaSocios[i] == null && !registrado) {
+                listaSocios[i] = s;
+                registrado = true;
+            }
+        }
+    return registrado;
+
+    }
+
+    public Socio expulsarSocio(int numero){
+        boolean expulsadoCorrecto = false;
+        Socio socio = null;
+        for (int i = 0; i < listaSocios.length; i++) {
+
+        }
+        return socio;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Gimnasio{" + "codigo=" + codigo + ", nombre=" + nombre + '}';
+    }
 }
